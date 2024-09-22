9ddd0abbb2f8e6c79e53b2bb5555d5ef9ef72130
Pattaradanai Kamsuan <pattaradanai4290@gmail.com>
2024-09-22 18:58:45 +0700

    Refactor unit_test.py to update assert statement for app.hello and app.plus methods

--- unit_test.py
+++ unit_test.py
@@ -22,33 +22,32 @@
 #     unittest.main()
 
 import unittest
+
 from app import app
 import json
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
-        # Set up the test client for Flask
-        self.app = app.test_client()
-        self.app.testing = True
-
+        self.app = app.test_client()  # Create a test client
+        self.app_context = app.app_context()  # Create an app context
+        self.app_context.push() 
+    
     def test_hello_with_string(self):
-        # Test hello route with a string
-        res = self.app.get('/hello/Rathachai')
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.get_data(as_text=True), "Hello, Rathachai")
+        # test hello
+        res = app.hello("Rathachai")
+        self.assertEqual(res, "Hello, Rathachai")
 
     def test_hello_with_number(self):
-        # Test hello route with a number
-        res = self.app.get('/hello/1')
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.get_data(as_text=True), "Hello, 1")
+        # test hello
+        res = app.hello(1)
+        self.assertEqual(res, "Hello, 1")
     
     def test_plus_json(self):
-        # Test plus route with two numbers
         res = self.app.get('/plus/5/6')
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(json.loads(res.get_data(as_text=True)), {'result': 11})
+        self.assertEqual(res.get_json(), 11)
 
 if __name__ == "__main__":
     unittest.main()
+
 
